452a6c1a854d0e561fb8587ab32d1eb4a52f1815
Disaster <Disaster20031017@gmail.com>
2024-09-07 13:05:55 +0900

    Update GameManager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -19,6 +19,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(this);
+
         state_scene = STATE_SCENE.TITLE;
     }
 
